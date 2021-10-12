1571e4c62f943f16d49db2dd96bb8570f10b2b0f
max <max@rtall.ru>
2021-10-12 16:54:26 +0300

    +keylist service parameters

--- src/sm_async_api/config.clj
+++ src/sm_async_api/config.clj
@@ -111,15 +111,18 @@
         workers-file (str path "workers.yml")
         db-file (str path "db.yml")
         keystore-file (str path "keystore.yml")
-        globals-file (str path "globals.yml")]
+        globals-file (str path "globals.yml")
+        messengers-file (str path "messengers.yml")]
     (with-local-vars [local-config* {}]
       (let [workers  (yaml/from-file workers-file)
             db (yaml/from-file db-file)
             base (yaml/from-file base-file)
-            globals (yaml/from-file globals-file)]
+            globals (yaml/from-file globals-file)
+            messengers (yaml/from-file messengers-file)]
         (when (and (some? workers) (some? base) (some? db))
           (reset! keystore (yaml/from-file keystore-file))
           (var-set local-config* (-> {} (assoc :workers (decoder workers))
+                                     (assoc :messengers  (if (some? messengers) messengers {}))
                                      (assoc :config (decoder base))
                                      (assoc :database (decoder db))
                                      (assoc :executors-globals
@@ -163,6 +166,28 @@
                        (string->b64 (str "Basic " (% :async-user) ":" (% :async-password)))))
               fill-workers)))
 
+(defn- fill-messengers [messengers]
+  (assoc messengers :dedicated
+         (for [w (messengers :dedicated)]
+           (->> w
+                (#(if (nil? (:threads %))
+                    (assoc % :threads (messengers :dedicated-threads)) %))
+                (#(if (nil? (:chank-size %))
+                    (assoc % :chank-size (messengers :dedicated-chank-size)) %))))))
+
+(defn- configure-messengers [global-configuration]
+  (assoc global-configuration :messengers
+         (->> global-configuration
+              :messengers
+              (#(if (nil? (% :dedicated-threads))
+                  (assoc % :dedicated-threads default-threads-per-manager)
+                  %))
+              (#(if (nil? (% :dedicated-chank-size))
+                  (assoc % :dedicated-chank-size default-lock-chunk-size)
+                  %))
+              fill-messengers)))
+
+
 (defn create-uid-generator [_config]
   (assoc  _config  :get-unique-id
           (let [i (atom 0)
@@ -173,6 +198,11 @@
                       (swap! i inc)
                       module-name)))))
 
+(defn collection-keylist-builder [config]
+  (assoc config :collections-keylist
+         (reduce #(if (and (some? (:name %2)) (some? (:keylist %2))) 
+                      (conj %1 {(keyword (:name %2))  (:keylist %2)})
+                      %1) {} (-> config :config :collections))))
 
 (defn configure [path]
   (reset! config
@@ -183,8 +213,9 @@
                 fill-config-defaults
                 calc-config-defaults
                 configure-workers
+                collection-keylist-builder
+                configure-messengers
                 create-uid-generator))))
-
 
 
 
@@ -205,9 +236,9 @@
   ([key] `(-> @sm_async_api.config/config :workers ~key)))
 
 
-(defmacro get-messangers
-  ([]  `(@sm_async_api.config/config :workers))
-  ([key] `(-> @sm_async_api.config/config :workers ~key)))
+(defmacro get-messengers
+  ([]  `(@sm_async_api.config/config :messengers))
+  ([key] `(-> @sm_async_api.config/config :messengers ~key)))
 
 
 (defmacro get-executors-globals
@@ -217,6 +248,11 @@
 (defmacro get-module-name [] `(-> @sm_async_api.config/config :config :async_gateway_id))
 
 (defmacro get-uid []  `((@sm_async_api.config/config  :get-unique-id)))
+
+
+(defmacro get-keylist [collection]
+  `(-> @sm_async_api.config/config :collections-keylist ~collection))
+
 
 
 ;(defn set-module-name [name] (reset! module_name name))
